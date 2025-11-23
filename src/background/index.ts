df55430ce22aa1ef103a04bded08d925d20992b6
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-11-23 17:45:30 +0000

    move background service into debug mode

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,5 +1,17 @@
+import { isDebug } from "../shared/debugger";
 import { logger } from "../shared/logger";
 
-chrome.runtime.onInstalled.addListener(() => {
-  logger.debug("Extension installed");
-});
+if (isDebug()) {
+  chrome.runtime.onInstalled.addListener(() => {
+    logger.debug("Extension installed");
+  });
+
+  chrome.runtime.onMessage.addListener((request, _sender, sendResponse) => {
+    logger.debug("Message received:", request);
+    if (request.msg) {
+      logger.debug("Message content:", request.msg);
+    }
+    sendResponse();
+    return true;
+  });
+}
