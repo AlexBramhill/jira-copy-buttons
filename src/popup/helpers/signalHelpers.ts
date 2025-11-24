2dff5bae5bc5a1d498b2e4370a460b0522543a1f
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-11-24 00:10:39 +0000

    wip restructure

--- src/popup/helpers/signalHelpers.ts
+++ src/popup/helpers/signalHelpers.ts
@@ -1,21 +1,37 @@
-export const removeFieldByIndexCallback = <T>(
-  indexToRemove: number
+import type { UUID } from "crypto";
+
+export interface ValueStoreWithId<T> {
+  id: UUID;
+  value: T;
+}
+
+export const removeFieldByIdCallback = <
+  TValue,
+  T extends ValueStoreWithId<TValue>
+>(
+  idToRemove: UUID
 ): ((current: T[]) => T[]) => {
   return (current) => {
     if (current.length === 1) {
       return [];
     }
-    return current.filter((_, i) => i !== indexToRemove);
+    return current.filter((item) => item.id !== idToRemove);
   };
 };
 
-export const updateFieldByIndexCallback = <T>(
-  indexToUpdate: number,
+export const updateFieldByIdCallback = <
+  TValue,
+  T extends ValueStoreWithId<TValue>
+>(
+  idToUpdate: UUID,
   newValue: T
 ): ((current: T[]) => T[]) => {
   return (current) => {
     const next = [...current];
-    next[indexToUpdate] = newValue;
+    const index = next.findIndex((item) => item.id === idToUpdate);
+    if (index !== -1) {
+      next[index] = newValue;
+    }
     return next;
   };
 };
