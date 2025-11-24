7acada027472de67ba401ffcd5d6e80bb24e3ce1
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-11-24 23:33:54 +0000

    add router

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,4 +1,13 @@
 import { render } from "solid-js/web";
+import { HashRouter, Route } from "@solidjs/router";
 import Popup from "./pages/Popup";
 
-render(() => <Popup />, document.getElementById("root")!);
+render(
+  () => (
+    <HashRouter>
+      <Route path="/" component={Popup} />
+      {/* <Route path="/add-button" component={AddBranchCopyButton} /> */}
+    </HashRouter>
+  ),
+  document.getElementById("root")!
+);
