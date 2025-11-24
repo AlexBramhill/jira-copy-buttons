43f7db56b0c754cb1e54d129a13c48ac0ddaf440
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-11-24 23:40:41 +0000

    add navbar

--- src/popup/pages/Popup.tsx
+++ src/popup/pages/Popup.tsx
@@ -4,13 +4,16 @@ import {
 } from "../../shared/repository/chromeStorageSync";
 import PersistentMultipleTextInputs from "../components/multiple-text-inputs/PersistentMultipleTextInputs";
 import PopupContainer from "../components/PopupContainer";
+import PopupNavbar from "../components/PopupNavbar";
 import UrlTextInput from "../components/text-inputs/UrlTextInput.tsx";
 
 export const Popup = () => {
   return (
     <PopupContainer>
-      <h1 class="text-2xl font-bold mb-4">Jira Branch Creator</h1>
-      <p>Adds helpful buttons for Jira issues to create branches easily.</p>
+      <PopupNavbar title="Jira Branch Creator" />
+      <p class="mb-4">
+        Adds helpful buttons for Jira issues to create branches easily.
+      </p>
       <PersistentMultipleTextInputs
         textInput={UrlTextInput}
         loadFromPersistence={getHostnames}
