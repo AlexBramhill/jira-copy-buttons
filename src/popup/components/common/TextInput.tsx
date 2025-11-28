f505f1879fce117435d36771ec29fc91db60e662
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-11-28 00:03:22 +0000

    fix input box css

--- src/popup/components/common/TextInput.tsx
+++ src/popup/components/common/TextInput.tsx
@@ -23,10 +23,10 @@ export const TextInput = (props: TextInputProps) => (
       value={props.value}
       onInput={(e) => props.onInput(e.currentTarget.value)}
       placeholder={props.placeholder}
-      class={`flex-1 min-w-0 rounded-none border-y border-neutral-700 bg-neutral-900 px-3 py-2 text-sm text-neutral-100 placeholder:text-neutral-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none ${
+      class={`flex-1 min-w-0 rounded-none border border-neutral-700 bg-neutral-900 px-3 py-2 text-sm text-neutral-100 placeholder:text-neutral-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-500 focus:outline-none ${
         props.class || ""
-      } ${props.prefix ? "rounded-l-none" : "rounded-l-md"} ${
-        props.suffix ? "rounded-r-none" : "rounded-r-md"
+      } ${props.prefix ? "rounded-l-none border-l-0" : "rounded-l-md"} ${
+        props.suffix ? "rounded-r-none border-r-0" : "rounded-r-md"
       }`}
       autocomplete="off"
     />
