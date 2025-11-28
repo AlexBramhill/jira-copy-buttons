8961046edc770b55142c10a597106fa037be1a0c
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-11-28 01:08:45 +0000

    add wip devtools page

---
+++ src/popup/components/feature/dev-tools/DevToolsManager.tsx
@@ -0,0 +1,7 @@
+import Accordion from "../../common/Accordion";
+
+export const DevToolsManager = () => {
+  return <Accordion header="Dev Tools"> Devtools</Accordion>;
+};
+
+export default DevToolsManager;
