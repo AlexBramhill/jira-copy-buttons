38d2260c6cacb4bd6efecdc32af4298f5b8f7db3
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-12-10 01:14:02 +0000

    remove unused manager

--- src/popup/components/feature/dev-tools/DevToolsManager.tsx
+++
@@ -1,7 +0,0 @@
-import Accordion from "../../common/Accordion";
-
-export const DevToolsManager = () => {
-  return <Accordion header="Dev Tools"> Devtools</Accordion>;
-};
-
-export default DevToolsManager;
