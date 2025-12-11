b645b041cbcaae26f0bf896bb9746a1f3f10d5b8
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-12-11 01:49:29 +0000

    add selector field prefixes, suffixes

--- src/popup/components/common/fields/ElementSelectorField.tsx
+++ src/popup/components/common/fields/ElementSelectorField.tsx
@@ -15,9 +15,22 @@ interface ElementSelectorFieldProps {
 
 // TODO constrain this type to require all SelectorTypes
 const selectorTypeOptions = [
-  { value: SelectorType.TestId, label: "Test ID (data-testid)" },
-  { value: SelectorType.VcAttribute, label: "VC Attribute (data-vc)" },
-  { value: SelectorType.CssSelector, label: "CSS Selector" },
+  {
+    value: SelectorType.TestId,
+    label: "Test ID Attribute (data-testid)",
+    prefix: '[data-testid="',
+    suffix: '"]',
+  },
+  {
+    value: SelectorType.VcAttribute,
+    label: "VC Attribute (data-vc)",
+    prefix: '[data-vc="',
+    suffix: '"]',
+  },
+  {
+    value: SelectorType.CssSelector,
+    label: "Other CSS Selector",
+  },
 ];
 
 export const ElementSelectorField = (props: ElementSelectorFieldProps) => {
@@ -25,17 +38,24 @@ export const ElementSelectorField = (props: ElementSelectorFieldProps) => {
     <FormField id={props.id} label={props.label}>
       <div class="space-y-2">
         <Select
+          prefix="Select by:"
           id={`${props.id}-type`}
           value={props.value.type}
-          onChange={(type) =>
-            props.onChange({ ...props.value, type: type })
-          }
+          onChange={(type) => props.onChange({ ...props.value, type: type })}
           options={selectorTypeOptions}
         />
         <TextInput
           id={`${props.id}-value`}
           value={props.value.value}
           onInput={(value) => props.onChange({ ...props.value, value })}
+          prefix={
+            selectorTypeOptions.find((opt) => opt.value === props.value.type)
+              ?.prefix
+          }
+          suffix={
+            selectorTypeOptions.find((opt) => opt.value === props.value.type)
+              ?.suffix
+          }
           placeholder={
             props.value.type === SelectorType.CssSelector
               ? ".my-class or [data-attr='value']"
